a481de9e0e41369ff503d709709d760ac3d71a4f
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2014-09-13 16:45:22 +0200

    feat(*): everything

---
+++ hexcolor-swizzle.js
@@ -0,0 +1,35 @@
+import hexcolor from 'hexcolor';
+
+var each = Function.prototype.call.bind(Array.prototype.forEach);
+
+var getters = { r: hexcolor.red, g: hexcolor.green, b: hexcolor.blue, a: hexcolor.alpha };
+
+each('rgb', function (r) {
+  each('rgb', function (g) {
+    each('rgb', function (b) {
+      var name = r + g + b;
+      Object.defineProperty(Number.prototype, name, {
+        get: new Function('rgba', 'r', 'g', 'b', 'return function ' + name + '() {' +
+          'return rgba(r(color), g(color), b(color), 255);' +
+        '}')(rgba, getters[r], getters[g], getters[b]),
+        enumerable: false
+      });
+    });
+  });
+});
+
+each('rgba', function (r) {
+  each('rgba', function (g) {
+    each('rgba', function (b) {
+      each('rgba', function (a) {
+        var name = r + g + b + a;
+        Object.defineProperty(Number.prototype, name, {
+          get: new Function('rgba', 'r', 'g', 'b', 'a', 'return function ' + name + '(color) {' +
+            'return rgba(r(color), g(color), b(color), a(color));' +
+          '}')(rgba, getters[r], getters[g], getters[b], getters[a]),
+          enumerable: false
+        });
+      });
+    });
+  });
+});
